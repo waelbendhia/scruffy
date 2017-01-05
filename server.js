81dbd8c70f2844c2482f06cc166a625717c111f6
WaelBenDhia <waelbendhia@gmail.com>
2017-01-05 16:38:47 +0100

    more more console logs for debugging deployment

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 console.log("process.env.OPENSHIFT_NODEJS_IP : " + process.env.OPENSHIFT_NODEJS_IP)
+console.log("process.env.OPENSHIFT_NODEJS_PORT : " + process.env.OPENSHIFT_NODEJS_PORT)
 console.log("Server on : " + ip + " " + port)
 
 server.use(bodyParser.json())
