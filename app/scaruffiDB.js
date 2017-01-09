649d65fafa4fad9c6471cdde6699ea81c96b0aee
WaelBenDhia <waelbendhia@gmail.com>
2017-01-09 17:31:02 +0100

    MySql now uses a pool of 100 connections

--- app/scaruffiDB.js
+++ app/scaruffiDB.js
@@ -5,31 +5,31 @@ var db_user = process.env.MYSQL_USER || "wael"
 var db_password = process.env.MYSQL_PASSWORD || ""
 var db_database = process.env.MYSQL_DATABASE || "scaruffi"
 
-console.log("process.env.MYSQL_HOST: " + process.env.MYSQL_HOST)
-console.log("process.env.MYSQL_USER " + process.env.MYSQL_USER)
-console.log(" process.env.MYSQL_PASSWORD:" + process.env.MYSQL_PASSWORD)
-console.log(" process.env.MYSQL_DATABASE:" + process.env.MYSQL_DATABASE)
-console.log("Db on : " + db_host + " user:" + db_user + " password:" + db_password + " name:" + db_database)
+const pool = mysql.createPool({
+	connectionLimit : 100,
+	host : db_host,
+	user : db_user,
+	password : db_password,
+	database : db_database,
+	debug : false
+});
 
-const con = mysql.createConnection({
-	host: db_host,
-	user: db_user,
-	password: db_password,
-	database: db_database
-})
+var getConnection = function(){
+	return new Promise(function(fulfill, reject){
+		pool.getConnection(function(err, connection){
+			if(err){
+				reject(err)
+				connection.release()
+			}else
+				fulfill(connection)
+		})
+	})
+}
 
 const SORT_BY_RATING = 0;
 const SORT_BY_DATE = 1;
 const SORT_BY_ALBUM_NAME = 2;
 const SORT_BY_BANDNAME = 3;
-
-con.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + con.threadId);
-})
 
 var parseBandFromRow = function(row){
 	var band = {
@@ -81,27 +81,37 @@ var getSortByAsString = function(sortBy, albumSymbol, bandSymbol){
 
 var getRelatedBands = function(band, callback){
 	var query = `select * from bands INNER JOIN bands2bands ON bands.partialUrl = bands2bands.urlOfRelated where bands2bands.urlOfBand ='${band.url}'`;
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		for( i = 0; i < rows.length; i++ ){
-			var relBand = parseBandFromRow(rows[i])
-			relBand.bio = ''
-			band.relatedBands.push(relBand)
-		}
-		callback(band)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			for( i = 0; i < rows.length; i++ ){
+				var relBand = parseBandFromRow(rows[i])
+				relBand.bio = ''
+				band.relatedBands.push(relBand)
+			}
+			callback(band)
+		})
+		con.release()
+	},function(err){
+		console.log(err)
 	})
 }
 
 var getAlbums = function(band, callback){
 	var query = `select * from albums where band ='${band.url}'`
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		for( i = 0; i < rows.length; i++ ){
-			band.albums.push(parseAlbumFromRow(rows[i]))
-		}
-		callback(band)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			for( i = 0; i < rows.length; i++ ){
+				band.albums.push(parseAlbumFromRow(rows[i]))
+			}
+			callback(band)
+		})
+		con.release()
+	},function(err){
+		console.log(err)
 	})
 }
 
@@ -115,49 +125,70 @@ module.exports.getBand = function(partialUrl, callback){
 		firstBand = band
 		getAlbums(firstBand, finalCallback)
 	}
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		firstBand = parseBandFromRow(rows[0])
-		getRelatedBands(firstBand, callbackFromRelatedBands)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			firstBand = parseBandFromRow(rows[0])
+			getRelatedBands(firstBand, callbackFromRelatedBands)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)
 	})
 }
 
 module.exports.getRatingDistribution = function(callback){
 	var query = `SELECT floor(albums.rating*2)/2 as rating, count(*) as count FROM albums group by floor(albums.rating*2)/2;`;
 	var disrib = {}
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		for(var i = 0; i < rows.length; i++){
-			var row = rows[i]
-			disrib[row.rating.toFixed(1)] = row.count
-		}
-		callback(disrib)
+	getConnection().then(function(con){
+		console.log("Result of getConnection : " + con)
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			for(var i = 0; i < rows.length; i++){
+				var row = rows[i]
+				disrib[row.rating.toFixed(1)] = row.count
+			}
+			callback(disrib)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)
 	})
 }
 
 module.exports.getBandCount = function(callback){
 	var query = `select count(*) as count FROM bands;`;
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		callback(rows[0].count)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			callback(rows[0].count)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)
 	})
 }
 
 module.exports.getBandsInfluential = function(callback){
 	var query = `select count(b2b.urlOfBand) as inf, b.name, b.partialUrl FROM bands b inner join bands2bands b2b on b.partialUrl = b2b.urlOfRelated group by b2b.urlOfRelated order by inf desc limit 21`;
 	var bands = []
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		for( i = 0; i < rows.length; i++ ){
-			var band = parseBandFromRow(rows[i])
-			band.bio = ''
-			bands.push(band)
-		}
-		callback(bands)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			for( i = 0; i < rows.length; i++ ){
+				var band = parseBandFromRow(rows[i])
+				band.bio = ''
+				bands.push(band)
+			}
+			callback(bands)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)	
 	})
 }
 
@@ -170,24 +201,25 @@ module.exports.searchAlbums = function(req, callback){
 		+ "order by " + getSortByAsString(req.sortBy, "a", "b") + (req.sortOrderAsc ? " asc " : " desc ") 
 		+ "limit " + (req.page * req.numberOfResults) + "," + req.numberOfResults + ";";
 
-	console.log("Request sort by: " + req.sortBy)
-	console.log("Search query: " + query)
-
 	var albums = []
-
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		for( i = 0; i < rows.length; i++ ){
-			var album = parseAlbumFromRow(rows[i])
-			album.band = {
-				name: rows[i].bandname,
-				url: rows[i].bandurl,
-				fullurl: `http://scaruffi.com/${rows[i].bandurl}`
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			for( i = 0; i < rows.length; i++ ){
+				var album = parseAlbumFromRow(rows[i])
+				album.band = {
+					name: rows[i].bandname,
+					url: rows[i].bandurl,
+					fullurl: `http://scaruffi.com/${rows[i].bandurl}`
+				}
+				albums.push(album)
 			}
-			albums.push(album)
-		}
-		callback(albums)
+			callback(albums)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)	
 	})
 }
 
@@ -198,11 +230,17 @@ module.exports.searchAlbumsCount = function(req, callback){
 		+ (req.includeUnknown ? " or a.year = 0" : "") + ") " 
 		+ (!req.name ? "" : "and ( instr(lower(a.name), lower('" + req.name + "')) or instr(lower(b.name), lower('" + req.name + "'))) ") + ";";
 
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		callback(rows[0].count)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			callback(rows[0].count)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)	
 	})
+
 }
 
 module.exports.searchBands = function(req, callback){
@@ -212,15 +250,19 @@ module.exports.searchBands = function(req, callback){
 				+ "limit " + (req.page * req.numberOfResults) + "," + req.numberOfResults + ";";
 
 	var bands = []
-
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		for( i = 0; i < rows.length; i++ ){
-			var band = parseBandFromRow(rows[i])
-			bands.push(band)
-		}
-		callback(bands)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			for( i = 0; i < rows.length; i++ ){
+				var band = parseBandFromRow(rows[i])
+				bands.push(band)
+			}
+			callback(bands)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)	
 	})
 }
 
@@ -228,9 +270,15 @@ module.exports.searchBandsCount = function(req, callback){
 	var query = "select count(*) as count from bands b where "
 				+ "instr(lower(b.name), lower('" + req.name + "')) " + ";";
 
-	con.query(query, function(err, rows){
-		if(err)
-			console.log(err)
-		callback(rows[0].count)
+	getConnection().then(function(con){
+		con.query(query, function(err, rows){
+			if(err)
+				console.log(err)
+			callback(rows[0].count)
+		})
+		con.release()
+	}, function(err){
+		console.log(err)	
 	})
+
 }
