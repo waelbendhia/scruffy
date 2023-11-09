e0e11adfd40f64a9e713db68c3e6c6f3ff241906
waelbendhia <waelbendhia@gmail.com>
2023-11-09 02:54:14 +0100

    missing semi colon

--- app/next-app/src/app/albums/search/route.ts
+++ app/next-app/src/app/albums/search/route.ts
@@ -2,7 +2,7 @@ import { API } from "@scruffy/server";
 import { client } from "@/api";
 
 export async function GET(request: Request) {
-  const queryString = new URL(request.url).search
+  const queryString = new URL(request.url).search;
   const params = new URLSearchParams(queryString);
   const resp = await client.get<API["/album"]["/"]>(`/album`, { params });
   return Response.json(resp.data);
