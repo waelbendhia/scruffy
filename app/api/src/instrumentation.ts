70c20fb1b1ceb42c4039cf117b07a572be90d1bf
waelbendhia <waelbendhia@gmail.com>
2023-12-08 14:49:47 +0100

    drop some otel instrumentation

--- app/api/src/instrumentation.ts
+++ app/api/src/instrumentation.ts
@@ -1,16 +1,12 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { PrismaInstrumentation } from "@prisma/instrumentation";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { FastifyInstrumentation } from "@opentelemetry/instrumentation-fastify";
 
 const sdk = new NodeSDK({
   serviceName: "scruffy-api",
   instrumentations: [
     getNodeAutoInstrumentations(),
     new PrismaInstrumentation(),
-    new HttpInstrumentation(),
-    new FastifyInstrumentation(),
   ],
 });
 
